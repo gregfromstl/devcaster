4cb4e3fc92de4b6d20f6661c5071017c5b85849f
gregfromstl <gregfromstl@gmail.com>
2024-02-01 23:45:46 -0800

    log frame

--- examples/next/photo-gallery/src/app/api/route.ts
+++ examples/next/photo-gallery/src/app/api/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+    console.log("IN API");
     return NextResponse.redirect(process.env.BASE_URL as string, {
         status: 302,
     });
