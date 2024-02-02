984082f708d3dc928d7960e9d9ff604755f9177a
gregfromstl <gregfromstl@gmail.com>
2024-02-02 00:05:29 -0800

    return to og middleware

--- examples/next/photo-gallery/src/middleware.ts
+++ examples/next/photo-gallery/src/middleware.ts
@@ -1,46 +1,45 @@
 import { NextResponse, type NextRequest } from "next/server";
-// import { framesMiddleware } from "@devcaster/next/frames";
-export const dynamic = "force-dynamic";
+import { framesMiddleware } from "@devcaster/next/frames";
 
-async function framesMiddleware(request: NextRequest) {
-    console.log(request.url);
-    const method = request.method;
-    const newUrl = new URL(request.url);
+// async function framesMiddleware(request: NextRequest) {
+//     console.log(request.url);
+//     const method = request.method;
+//     const newUrl = new URL(request.url);
 
-    if (
-        method === "POST" &&
-        !parseInt(newUrl.searchParams.get("request_complete") ?? "0")
-    ) {
-        const body = await request.json();
-        console.log("body", body);
+//     if (
+//         method === "POST" &&
+//         !parseInt(newUrl.searchParams.get("request_complete") ?? "0")
+//     ) {
+//         const body = await request.json();
+//         console.log("body", body);
 
-        const redirects = newUrl.searchParams.get("redirects");
-        if (redirects) {
-            console.log("redirects", redirects);
-            const redirectsObj = JSON.parse(decodeURIComponent(redirects));
-            if (redirectsObj[`${body.untrustedData.buttonIndex}`]) {
-                const redirectUrl = new URL(
-                    redirectsObj[`${body.untrustedData.buttonIndex}`]
-                );
-                redirectUrl.searchParams.set("request_complete", "1");
-                console.log("redirecting to", redirectUrl.toString());
-                return NextResponse.redirect(redirectUrl.toString(), {
-                    status: 302,
-                });
-            }
-        }
-        newUrl.searchParams.set(
-            "action",
-            encodeURIComponent(JSON.stringify(body))
-        );
-    }
+//         const redirects = newUrl.searchParams.get("redirects");
+//         if (redirects) {
+//             console.log("redirects", redirects);
+//             const redirectsObj = JSON.parse(decodeURIComponent(redirects));
+//             if (redirectsObj[`${body.untrustedData.buttonIndex}`]) {
+//                 const redirectUrl = new URL(
+//                     redirectsObj[`${body.untrustedData.buttonIndex}`]
+//                 );
+//                 redirectUrl.searchParams.set("request_complete", "1");
+//                 console.log("redirecting to", redirectUrl.toString());
+//                 return NextResponse.redirect(redirectUrl.toString(), {
+//                     status: 302,
+//                 });
+//             }
+//         }
+//         newUrl.searchParams.set(
+//             "action",
+//             encodeURIComponent(JSON.stringify(body))
+//         );
+//     }
 
-    newUrl.searchParams.set(
-        "url",
-        encodeURIComponent(process.env.BASE_URL ?? newUrl.origin)
-    );
-    return NextResponse.rewrite(newUrl);
-}
+//     newUrl.searchParams.set(
+//         "url",
+//         encodeURIComponent(process.env.BASE_URL ?? newUrl.origin)
+//     );
+//     return NextResponse.rewrite(newUrl);
+// }
 
 export async function middleware(request: NextRequest) {
     return await framesMiddleware(request);
